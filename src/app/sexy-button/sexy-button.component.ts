5af1cf0fe7faac020af9078fc5e7f8c6e42af434
Saransh Gupta <saranshgupta1995@gmail.com>
2018-09-13 21:08:56 +0530

    made sexy button sexier

--- src/app/sexy-button/sexy-button.component.ts
+++ src/app/sexy-button/sexy-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'sexy-button',
@@ -7,22 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SexyButtonComponent implements OnInit {
 
-    tilted=0;
+    tilted = 0;
+    powerTimer;
+    @ViewChild('AwesomeTextSpan') textSpan;
 
-    constructor() { }
+    constructor() { 
+    }
+
+    animatePowerBar(width, td) {
+        document.getElementsByClassName('power-up-bg')[0]['style'].transitionDuration = td;
+        document.getElementsByClassName('power-up-bg')[0]['style'].width = width;
+    }
 
     initiateAnimation(event) {
-        let that=this;
-        event.target.onmousemove=function(e){
-            let newAngle = ((e.pageX - this.offsetLeft) - this.offsetWidth / 2)/60;
-            that.tilted=newAngle<0?-1:1;
-            that.tilted = Math.abs(newAngle) <0.5?0:that.tilted;
-            // event.target.style.transform = `skewY(${newAngle}deg) translate3d(0, -.5px, 0)`;
+        let that = this;
+        event.target.onmousemove = function (e) {
+            let newAngle = ((e.pageX - this.offsetLeft) - this.offsetWidth / 2) / 60;
+            that.tilted = newAngle < 0 ? -1 : 1;
+            that.tilted = Math.abs(newAngle) < 0.5 ? 0 : that.tilted;
+            if ((!that.tilted) && (!that.powerTimer)) {
+                that.animatePowerBar('100%', '2s');
+            } else if(that.tilted) {
+                that.animatePowerBar('0', '0.1s');
+                if (that.powerTimer) {
+                    clearInterval(that.powerTimer);
+                    that.powerTimer=null;
+                }
+            }
         }
     }
 
-    stopAnimation(){
-        this.tilted=0;
+    crazyShow(text:string){
+        let x = 0;
+        let y=text.length;
+        let that=this;
+        let markers=['_',' ']
+        let intervalID:any = setInterval(function () {
+            that.textSpan.nativeElement.innerText=text.slice(0,x)+markers[x%2];
+            if (++x === y+1) {
+                window.clearInterval(intervalID);
+            }
+        }, 300);
+    }
+
+    throwClick(){
+        this.crazyShow('Loading')
+        // let targetElem = document.getElementsByClassName('sexy-button')[0];
+        // targetElem['style']['top'] = (-this.poweredUp).toString()+'px';
+    }
+
+    stopAnimation() {
+        this.tilted = 0;
+        clearInterval(this.powerTimer);
+        this.powerTimer = null;
+        this.animatePowerBar('0', '0.1s');
+
     }
 
     ngOnInit() {
